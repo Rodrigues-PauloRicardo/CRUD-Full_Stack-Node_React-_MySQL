9e4d9608a2179516a2d1035521941f89f29c82e8
Rodrigues-PauloRicardo <rodrigues.pauloricardo@gmail.com>
2023-02-21 11:09:30 -0300

    at

---
+++ front/src/styles/global.js
@@ -0,0 +1,20 @@
+import {createGlobalStyle} from "styled-components"
+
+const Global = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'poppins', sans-serif;
+}
+
+body {
+    margin-top:10%;
+    width: 100vw;
+    height: 100 vh;
+    display: flex;
+    justify-content: center;
+    background-color: #f2f2f2;
+}
+`;
+
+export default Global;
